2535f8c808159d6f95bb04f03a8fc597591dcb72
TomoriFarouk <50277985+TomoriFarouk@users.noreply.github.com>
2021-09-02 19:47:06 +0100

    New
    New

---
+++ src/components/Body.vue
@@ -0,0 +1,161 @@
+<template>
+<div>
+     <section class="u-align-left u-clearfix u-section-1" id="sec-d882">
+      <div class="u-clearfix u-layout-wrap u-layout-wrap-1">
+        <div class="u-layout">
+          <div class="u-layout-row">
+            <div class="u-container-style u-layout-cell u-size-29 u-layout-cell-1">
+              <div class="u-container-layout u-container-layout-1">
+                <h1 class="u-align-center-xs u-text u-text-1">Quick &amp; reliable <span class="u-text-palette-3-light-1">Warehousing </span>
+                  <span class="u-text-palette-3-light-1">and logistics</span> solution 
+                </h1>
+                <p class="u-align-center-xs u-text u-text-2">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                <a href="https://nicepage.com/c/art-design-html-templates" class="u-border-none u-btn u-btn-round u-button-style u-custom-color-1 u-hover-palette-1-light-1 u-radius-6 u-btn-1">Button</a>
+                <a href="https://nicepage.com/c/art-design-html-templates" class="u-border-2 u-border-white u-btn u-btn-round u-button-style u-none u-radius-50 u-text-custom-color-1 u-btn-2">Play Demo</a>
+                <img class="u-image u-image-default u-preserve-proportions u-image-1" src="../assets/images/Group692111.svg" alt="" data-image-width="107" data-image-height="107">
+                <img class="u-image u-image-default u-image-2" src="../assets/images/Ellipse12.svg" alt="" data-image-width="492" data-image-height="831">
+                <img class="u-image u-image-default u-preserve-proportions u-image-3" src="../assets/images/Group6920.svg" alt="" data-image-width="110" data-image-height="110">
+              </div>
+            </div>
+            <div class="u-container-style u-layout-cell u-size-31 u-layout-cell-2">
+              <div class="u-container-layout u-container-layout-2">
+                <img class="u-image u-image-default u-preserve-proportions u-image-4" src="../assets/images/sun.svg" alt="" data-image-width="110" data-image-height="110">
+                <img class="u-image u-image-5" src="../assets/images/91fa59e6781adbdced82e349bb595d991.png" data-image-width="700" data-image-height="594">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img class="u-image u-image-default u-preserve-proportions u-image-6" src="../assets/images/Group6922.svg" alt="" data-image-width="110" data-image-height="110">
+      <div class="u-border-2 u-border-white u-form u-radius-31 u-form-1">
+        <form action="#" method="POST" class="u-clearfix u-form-horizontal u-form-spacing-15 u-inner-form" style="padding: 36px;" source="custom">
+          <div class="u-form-group u-form-name">
+            <label for="name-558c" class="u-custom-font u-font-lato u-form-control-hidden u-label">Name</label>
+            <input type="text" placeholder="Enter Location" id="name-558c" name="name" class="u-border-1 u-border-grey-30 u-custom-font u-font-lato u-input u-input-rectangle u-radius-11" required="">
+          </div>
+          <div class="u-form-email u-form-group">
+            <label for="email-558c" class="u-custom-font u-font-lato u-form-control-hidden u-label">Email</label>
+            <input type="email" placeholder="Enter Destination" id="email-558c" name="email" class="u-border-1 u-border-grey-30 u-custom-font u-font-lato u-input u-input-rectangle u-radius-11" required="">
+          </div>
+          <div class="u-form-group u-form-group-3">
+            <label for="text-8fbc" class="u-custom-font u-font-lato u-form-control-hidden u-label"></label>
+            <input type="text" placeholder="Weight" id="text-8fbc" name="text" class="u-border-1 u-border-grey-30 u-custom-font u-font-lato u-input u-input-rectangle u-radius-11">
+          </div>
+          <div class="u-form-group u-form-submit">
+            <a href="#" class="u-border-none u-btn u-btn-round u-btn-submit u-button-style u-custom-color-3 u-radius-11 u-btn-3">Check Price<br>
+            </a>
+            <input type="submit" value="submit" class="u-form-control-hidden">
+          </div>
+          <div class="u-form-send-message u-form-send-success">Thank you! Your message has been sent.</div>
+          <div class="u-form-send-error u-form-send-message">Unable to send your message. Please fix errors then try again.</div>
+          <input type="hidden" value="" name="recaptchaResponse">
+        </form>
+      </div>
+    </section>
+    <section class="u-clearfix u-custom-color-2 u-section-2" id="sec-4175">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-gutter-38 u-layout-wrap u-layout-wrap-1">
+          <div class="u-gutter-0 u-layout">
+            <div class="u-layout-col">
+              <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-1">
+                <div class="u-container-layout u-container-layout-1">
+                  <h2 class="u-custom-font u-font-lato u-text u-text-default u-text-1">Services we offer</h2>
+                  <img class="u-image u-image-default u-preserve-proportions u-image-1" src="../assets/images/Group6928.png" alt="" data-image-width="201" data-image-height="211">
+                </div>
+              </div>
+              <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-2">
+                <div class="u-container-layout u-container-layout-2">
+                  <img class="u-image u-image-default u-preserve-proportions u-image-2" src="../assets/images/Group6922.svg" alt="" data-image-width="110" data-image-height="110">
+                  <h3 class="u-custom-font u-font-lato u-text u-text-custom-color-3 u-text-2">Global Frieght</h3>
+                  <h3 class="u-custom-font u-font-lato u-text u-text-custom-color-3 u-text-3">Global Frieght</h3>
+                  <img class="u-image u-image-default u-preserve-proportions u-image-3" src="../assets/images/Group6920.svg" alt="" data-image-width="110" data-image-height="110">
+                  <h3 class="u-custom-font u-font-lato u-text u-text-custom-color-3 u-text-4">Warehousing Services</h3>
+                  <img class="u-image u-image-default u-preserve-proportions u-image-4" src="../assets/images/Group6921.svg" alt="" data-image-width="110" data-image-height="110">
+                  <p class="u-text u-text-custom-color-4 u-text-5">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                  <p class="u-text u-text-custom-color-4 u-text-6">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                  <p class="u-text u-text-custom-color-4 u-text-7">Sample text. Click to select the text box. Click again or double click to start editing .</p>
+                  <a href="https://nicepage.com/c/art-design-html-templates" class="u-border-none u-btn u-btn-round u-button-style u-custom-color-1 u-custom-font u-font-lato u-hover-palette-1-light-1 u-radius-6 u-btn-1">Join Now</a>
+                  <a href="https://nicepage.com/c/art-design-html-templates" class="u-border-2 u-border-custom-color-3 u-btn u-btn-round u-button-style u-custom-color-2 u-custom-font u-font-lato u-hover-palette-1-light-1 u-radius-6 u-btn-2">Request Quote</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-clearfix u-section-3" id="sec-9fa6">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <img class="u-image u-image-default u-preserve-proportions u-image-1" src="../assets/images/Group6928.png" alt="" data-image-width="201" data-image-height="211">
+        <h3 class="u-custom-font u-font-lato u-text u-text-default u-text-1">
+          <span style="font-weight: 700;">Operation</span> Mode
+        </h3>
+        <div class="u-clearfix u-expanded-width u-gutter-28 u-layout-wrap u-layout-wrap-1">
+          <div class="u-gutter-0 u-layout">
+            <div class="u-layout-col">
+              <div class="u-size-20">
+                <div class="u-layout-row">
+                  <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-1">
+                    <div class="u-container-layout u-container-layout-1">
+                      <h3 class="u-custom-font u-font-lato u-text u-text-custom-color-5 u-text-default u-text-2">Connect</h3>
+                      <a href="https://nicepage.com/c/blog-posts-website-mockup" class="u-border-1 u-border-hover-palette-1-base u-border-palette-1-base u-btn u-btn-round u-button-style u-custom-color-6 u-hover-palette-1-base u-radius-50 u-btn-1">1</a>
+                      <p class="u-text u-text-custom-color-4 u-text-3">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                    </div>
+                  </div>
+                  <div class="u-container-style u-image u-layout-cell u-size-30 u-image-2" data-image-width="577" data-image-height="433">
+                    <div class="u-container-layout u-container-layout-2"></div>
+                  </div>
+                </div>
+              </div>
+              <div class="u-size-20">
+                <div class="u-layout-row">
+                  <div class="u-container-style u-layout-cell u-size-20 u-layout-cell-3">
+                    <div class="u-container-layout u-container-layout-3">
+                      <img class="u-absolute-hcenter-xs u-expanded u-image u-image-default" src="../assets/images/v247_604.png" alt="" data-image-width="1200" data-image-height="900">
+                    </div>
+                  </div>
+                  <div class="u-container-style u-layout-cell u-size-20 u-layout-cell-4">
+                    <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-sm u-valign-middle-xl u-container-layout-4">
+                      <img class="u-image u-image-default u-image-4" src="../assets/images/Group7030.svg" alt="" data-image-width="603" data-image-height="609">
+                    </div>
+                  </div>
+                  <div class="u-container-style u-layout-cell u-size-20 u-layout-cell-5">
+                    <div class="u-container-layout u-container-layout-5">
+                      <h3 class="u-custom-font u-font-lato u-text u-text-custom-color-5 u-text-default u-text-4">Store</h3>
+                      <a href="https://nicepage.com/c/blog-posts-website-mockup" class="u-border-1 u-border-hover-palette-1-base u-border-palette-1-base u-btn u-btn-round u-button-style u-custom-color-6 u-hover-palette-1-base u-radius-50 u-btn-2">2</a>
+                      <p class="u-text u-text-custom-color-4 u-text-5">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="u-size-20">
+                <div class="u-layout-row">
+                  <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-6">
+                    <div class="u-container-layout u-container-layout-6">
+                      <a href="https://nicepage.com/c/blog-posts-website-mockup" class="u-border-1 u-border-hover-palette-1-base u-border-palette-1-base u-btn u-btn-round u-button-style u-custom-color-6 u-hover-palette-1-base u-radius-50 u-btn-3">2</a>
+                      <h3 class="u-text u-text-custom-color-5 u-text-default u-text-6">Ship</h3>
+                      <p class="u-text u-text-custom-color-4 u-text-7">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                    </div>
+                  </div>
+                  <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-7">
+                    <div class="u-container-layout u-container-layout-7">
+                      <img class="u-expanded-height-lg u-expanded-height-md u-expanded-height-sm u-expanded-height-xl u-expanded-width-xs u-image u-image-default u-image-5" src="../assets/images/91fa59e6781adbdced82e349bb595d991.png" alt="" data-image-width="700" data-image-height="594">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-clearfix u-custom-color-7 u-section-4" id="sec-3ef1">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h3 class="u-custom-font u-font-lato u-text u-text-default u-text-1"><b>Warehouse <span style="font-weight: 400;">Onsite</span></b>
+        </h3>
+        <img class="u-expanded-width u-image u-image-default u-image-1" src="../assets/images/Group7029.svg" alt="" data-image-width="1440" data-image-height="1035">
+        <a href="https://nicepage.com/c/art-design-html-templates" class="u-border-none u-btn u-btn-round u-button-style u-custom-color-1 u-custom-font u-font-lato u-hover-palette-1-light-1 u-radius-6 u-btn-1">Join Now</a>
+        <a href="https://nicepage.com/c/art-design-html-templates" class="u-border-2 u-border-custom-color-3 u-btn u-btn-round u-button-style u-custom-color-2 u-custom-font u-font-lato u-hover-palette-1-light-1 u-radius-6 u-btn-2">Request Quote</a>
+      </div>
+    </section>
+</div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+    <div> 
+        <header class="u-clearfix u-header u-header" id="sec-8aae"><div class="u-clearfix u-sheet u-sheet-1">
+        <nav class=" u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</symbol>
+</defs></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Company.html" style="padding: 10px 12px;">Company</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Services.html" style="padding: 10px 12px;">Services</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Solutions.html" style="padding: 10px 12px;">Solutions</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" style="padding: 10px 12px;">Industries</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" style="padding: 10px 12px;">Insights</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" style="padding: 10px 12px;">News and Media</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Company.html" style="padding: 10px 12px;">Company</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Services.html" style="padding: 10px 12px;">Services</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Solutions.html" style="padding: 10px 12px;">Solutions</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" style="padding: 10px 12px;">Industries</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" style="padding: 10px 12px;">Insights</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" style="padding: 10px 12px;">News and Media</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+        <a href="https://nicepage.com/k/dog-website-templates" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-none u-radius-6 u-text-body-color u-text-hover-white u-btn-1">Button</a>
+        <a href="https://nicepage.com/k/dog-website-templates" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-custom-color-1 u-hover-palette-2-base u-radius-6 u-text-body-color u-text-hover-white u-btn-2">Button</a>
+        <p class="u-text u-text-default u-text-1">Ship<span class="u-text-palette-3-light-1">Up</span>
+        </p>
+      </div></header>
+    </div>
+</template>
